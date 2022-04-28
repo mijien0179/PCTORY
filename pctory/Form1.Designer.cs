18f15366ca59b9a1da535b6ef4b0cd77dd7faad4
Min <mijien0179@gmail.com>
2022-04-28 22:58:35 +0900

    basic viewer 생성

--- pctory/Form1.Designer.cs
+++ pctory/Form1.Designer.cs
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- pctory/Form1.cs
+++ pctory/Form1.cs
@@ -8,16 +8,135 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-
+using System.Threading;
+using System.IO;
 
 namespace pctory
 {
     public partial class Form1 : Form
     {
+        Tracer tracer = null;
+        Thread thread;
+
+        private bool running = false;
+
+        private void textUpdate()
+        {
+            List<DataGridViewRow> row = new List<DataGridViewRow>();
+            while (running)
+            {
+                row.Clear();
+                foreach (var key in tracer.ProcInfoList.GetKeys())
+                {
+                    var value = tracer.ProcInfoList.GetData(key).Last();
+
+                    DataGridViewRow temp = new DataGridViewRow();
+                    temp.Cells.AddRange(
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = Path.GetFileName(key)
+                        },
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = value.StartTime.ToString()
+                        },
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = value.EndTime == DateTime.MinValue ? "-" : value.EndTime.ToString()
+                        },
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = tracer.ProcInfoList.GetData(key).Count()
+                        }
+                    ); ;
+
+                    row.Add(temp);
+                }
+
+                if (InvokeRequired)
+                {
+                    Invoke(new MethodInvoker( () =>
+                    {
+                        InvalidateText(ref row);
+                    }));
+
+                }
+                else
+                {
+                    InvalidateText(ref row);
+                }
+
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+        private void InvalidateText(ref List<DataGridViewRow> data)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.AddRange(data.ToArray());
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+            tracer = new Tracer().RunTrace();
+
+            running = true;
+            thread = new Thread(textUpdate);
+            thread.Start();
+
+            DataGridView dv = this.dataGridView1;
+
+            DataGridViewColumn[] col = new DataGridViewColumn[4]{
+                new DataGridViewTextBoxColumn(){
+                    HeaderText = "프로그램",
+                    Name = "dvgColProgram",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width= 200
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "최근 접근 시작 기록",
+                    Name = "dvgColRecentAccess",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width = 250
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "최근 접근 종료 기록",
+                    Name = "dvgColRecentExit",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width = 250
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "접근 횟수",
+                    Name = "dvgColAccessCount",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width = 250
+                }
+            };
+
+            dv.Columns.AddRange(col);
         }
 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+
+
+            if (thread == null) return;
+
+            if (thread.Join(10000)) MessageBox.Show("스레드 정상 정료 성공");
+            else
+            {
+                MessageBox.Show("스레드 종료 대기 만료");
+                thread.Abort();
+                MessageBox.Show("스레드 강제 종료");
+            }
+        }
     }
 }
