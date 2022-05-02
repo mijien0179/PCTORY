f1cccf3f4fa6266121157b3e284f7ad02c89053e
김명석 <twinaudtjr@kw.ac.kr>
2022-05-02 15:34:18 +0900

    사용자 빈도수 체크 그래프
    사용자 빈도수 체크 그래프 생성

--- pctory/Form1.cs
+++ pctory/Form1.cs
@@ -12,9 +12,18 @@ namespace pctory
 {
     public partial class Form1 : Form
     {
+        Tracer tracer = new Tracer(true);
         public Form1()
         {
             InitializeComponent();
+            tracer.RunTrace();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graph grap = new Graph(tracer.ProcInfoList);
+            grap.Owner = this;
+            grap.Show();
         }
     }
 }
