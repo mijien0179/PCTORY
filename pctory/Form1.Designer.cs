2eee20aed0e3f043e50ce113e8142bbbc9134794
harry0625@office.kw.ac.kr <harry0625@office.kw.ac.kr>
2022-05-16 13:31:48 +0900

    main UI 작성 및 기능 구현 수정

--- pctory/Form1.Designer.cs
+++ pctory/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.시작ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.통계ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.주간그래프ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.월간그래프ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.월간그래프ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작ToolStripMenuItem,
+            this.통계ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1148, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 시작ToolStripMenuItem
+            // 
+            this.시작ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiStart,
+            this.tsmiStop,
+            this.tsmiSave});
+            this.시작ToolStripMenuItem.Name = "시작ToolStripMenuItem";
+            this.시작ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.시작ToolStripMenuItem.Text = "시작";
+            // 
+            // tsmiStart
+            // 
+            this.tsmiStart.Name = "tsmiStart";
+            this.tsmiStart.Size = new System.Drawing.Size(224, 26);
+            this.tsmiStart.Text = "시작(재시작)";
+            this.tsmiStart.Click += new System.EventHandler(this.tsmiStart_Click);
+            // 
+            // tsmiStop
+            // 
+            this.tsmiStop.Name = "tsmiStop";
+            this.tsmiStop.Size = new System.Drawing.Size(224, 26);
+            this.tsmiStop.Text = "중지";
+            this.tsmiStop.Click += new System.EventHandler(this.tsmiStop_Click);
+            // 
+            // tsmiSave
+            // 
+            this.tsmiSave.Name = "tsmiSave";
+            this.tsmiSave.Size = new System.Drawing.Size(224, 26);
+            this.tsmiSave.Text = "저장";
+            // 
+            // 통계ToolStripMenuItem
+            // 
+            this.통계ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.주간그래프ToolStripMenuItem,
+            this.월간그래프ToolStripMenuItem,
+            this.월간그래프ToolStripMenuItem1});
+            this.통계ToolStripMenuItem.Name = "통계ToolStripMenuItem";
+            this.통계ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.통계ToolStripMenuItem.Text = "통계";
+            // 
+            // 주간그래프ToolStripMenuItem
+            // 
+            this.주간그래프ToolStripMenuItem.Name = "주간그래프ToolStripMenuItem";
+            this.주간그래프ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.주간그래프ToolStripMenuItem.Text = "일간그래프";
+            // 
+            // 월간그래프ToolStripMenuItem
+            // 
+            this.월간그래프ToolStripMenuItem.Name = "월간그래프ToolStripMenuItem";
+            this.월간그래프ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.월간그래프ToolStripMenuItem.Text = "주간그래프";
+            // 
+            // 월간그래프ToolStripMenuItem1
+            // 
+            this.월간그래프ToolStripMenuItem1.Name = "월간그래프ToolStripMenuItem1";
+            this.월간그래프ToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.월간그래프ToolStripMenuItem1.Text = "월간그래프";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1148, 490);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1148, 520);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "PCTORY";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 시작ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiStart;
+        private System.Windows.Forms.ToolStripMenuItem tsmiStop;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSave;
+        private System.Windows.Forms.ToolStripMenuItem 통계ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 주간그래프ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 월간그래프ToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem 월간그래프ToolStripMenuItem1;
     }
 }
--- pctory/Form1.cs
+++ pctory/Form1.cs
@@ -2,19 +2,138 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace pctory
 {
     public partial class Form1 : Form
     {
+        Tracer tracer = null;
+        Thread thread;
+
+        private bool running = false;
+
+        private void textUpdate()
+        {
+            List<DataGridViewRow> row = new List<DataGridViewRow>();
+            while (running)
+            {
+                row.Clear();
+                foreach (var key in tracer.ProcInfoList.GetKeys())
+                {
+                    var value = tracer.ProcInfoList.GetData(key).Last();
+
+                    DataGridViewRow temp = new DataGridViewRow();
+                    temp.Cells.AddRange(
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = Path.GetFileName(key)
+                        },
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = value.StartTime.ToString()
+                        },
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = value.EndTime == DateTime.MinValue ? "-" : value.EndTime.ToString()
+                        },
+                        new DataGridViewTextBoxCell()
+                        {
+                            Value = tracer.ProcInfoList.GetData(key).Last().GetWindowTitle().Item2
+                        }
+                      
+                    );
+
+                    row.Add(temp);
+                }
+
+                if (dataGridView1.InvokeRequired)
+                {
+                    dataGridView1.Invoke(new MethodInvoker(() =>
+                    {
+                        InvalidateText(ref row);
+                    }));
+
+                }
+                else
+                {
+                    InvalidateText(ref row);
+                }
+
+                Thread.Sleep(3000);
+            }
+        }
+
+        private void InvalidateText(ref List<DataGridViewRow> data)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.AddRange(data.ToArray());
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+            tracer.StopTrace();
+
+            if (thread == null) return;
+        }
+
+        private void tsmiStart_Click(object sender, EventArgs e)
+        {
+            tracer = new Tracer(true).RunTrace();
+
+            running = true;
+            thread = new Thread(textUpdate);
+            thread.Start();
+
+            DataGridView dv = this.dataGridView1;
+
+            DataGridViewColumn[] col = new DataGridViewColumn[]{
+                new DataGridViewTextBoxColumn(){
+                    HeaderText = "프로그램",
+                    Name = "dvgColProgram",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width= 200
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "최근 접근 시작 기록",
+                    Name = "dvgColRecentAccess",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width = 250
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "최근 접근 종료 기록",
+                    Name = "dvgColRecentExit",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width = 250
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "최근 제목",
+                    Name = "dvgColRecentText",
+                    CellTemplate = new DataGridViewTextBoxCell(),
+                    Width = 250
+                }
+            };
+
+            dv.Columns.AddRange(col);
+        }
+
+        private void tsmiStop_Click(object sender, EventArgs e)
+        {
+            running = false;
+            tracer.StopTrace();
         }
     }
 }
