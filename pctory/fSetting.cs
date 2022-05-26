3b8c5fe891b69b8444ec20b2593fcd2a79d718e8
Min <mijien0179@gmail.com>
2022-05-26 13:35:52 +0900

    설정 폼 추가

---
+++ pctory/fSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pctory
+{
+    public partial class fSetting : pctory.design.pctoryForm
+    {
+        public fSetting()
+        {
+            InitializeComponent();
+        }
+    }
+}
